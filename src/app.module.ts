aaef4c419f2740313bc25b6e309a8ebbfd949ee4
Netanel kalir <netanelkalir@gmail.com>
2025-07-28 15:23:19 +0300

    basic moduls

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssignmentsModule } from './assignments/assignments.module';
+import { ShiftsModule } from './shifts/shifts.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AssignmentsModule, ShiftsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
