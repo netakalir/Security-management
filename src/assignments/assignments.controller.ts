861d70fa5c66f3d2c868622c4b3d426ea3061259
Netanel kalir <netanelkalir@gmail.com>
2025-07-28 15:58:42 +0300

    basic controller and servise

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AssignmentsService } from './assignments.service';
 
 @Controller('assignments')
-export class AssignmentsController {}
+export class AssignmentsController {
+    constructor(private readonly assignmentsService: AssignmentsService) { }
+
+    @Get()
+    getAns(): string {
+        return this.assignmentsService.getHello()
+    }
+}
--- src/assignments/assignments.service.ts
+++ src/assignments/assignments.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AssignmentsService {}
+export class AssignmentsService {
+    getHello(): string {
+    return 'Hello soldier!';
+  }
+}
