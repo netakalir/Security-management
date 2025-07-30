14e2f211a56aa627587ec320d64364b6e1b7cf7d
Netanel kalir <netanelkalir@gmail.com>
2025-07-30 14:52:13 +0300

    fase 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AssignmentsModule } from './assignments/assignments.module';
-import { ShiftsModule } from './shifts/shifts.module';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AssignmentsModule, ShiftsModule, UsersModule, AuthModule],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
